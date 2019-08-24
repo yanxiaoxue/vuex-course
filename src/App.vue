bf8356d735e78c4f828bc1a5955905bb55c33178
yanxiaoxue <15513296852@163.com>
2019-08-24 15:24:01 +0800

    'lesson-1'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>App.vue : {{count}}</p>
+    <p>App.vue : {{todos}}</p>
+    <Count/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Count from './components/Count';
+import {mapState} from 'vuex';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Count
+  },
+  // 写法3
+  // computed:mapState(["count","todos"]),
+
+  computed:{
+     ...mapState(["count","todos"]),
+     others:{
+
+     }
   }
+  // 写法二
+  // computed:mapState({
+  //   count:state => state.count,
+  //   todos:state => state.todos
+  // })
+  // 写法1
+  // computed:{
+  //   // 返回的是一个属性
+  //   count(){
+  //       return this.$store.state.count;
+  //   },
+  //   todos(){
+  //     return this.$store.state.todos;
+  //   }
+  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Count.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="count">
+    <div>
+        <hr/>
+        <h1>{{count}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'count',
+  components: {
+    
+  },
+  computed:{
+    // 返回的是一个属性
+    count(){
+        return this.$store.state.count;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
